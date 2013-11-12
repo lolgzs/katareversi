ab82a3af43dd947d85a03e7396f5eed2e665c764
Laurent Laffont <laurent.laffont@gmail.com>
2013-11-12 10:46:30 +0100

    Refactoring des tests en utilisant JUnit Parametized tests ( https://github.com/junit-team/junit/wiki/Parameterized-tests )

--- tests/InitialBoardTurnToBlackTest.java
+++ tests/InitialBoardTurnToBlackTest.java
@@ -1,63 +1,74 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class InitialBoardTurnToBlackTest {
+	@Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+        		{ 	"........\n" +
+        			"........\n" +
+        			"........\n" +
+        			"...BW...\n" +
+        			"...WB...\n" +
+        			"........\n" +
+        			"........\n" +
+        			"........\n" +
+        			"B",
+        			
+        			"........\n" +
+        			"........\n" +
+        			"....0...\n" +
+        			"...BW0..\n" +
+        			"..0WB...\n" +
+        			"...0....\n" +
+        			"........\n" +
+        			"........\n" +
+        			"B" },
+        			
+        			
+        			
+        			
+               	{ 	"........\n" +
+                	"........\n" +
+                	"........\n" +
+                	"...BW...\n" +
+                	"...WB...\n" +
+                	"........\n" +
+                	"........\n" +
+                	"........\n" +
+                	"B",
+                			
+                	"........\n" +
+                	"........\n" +
+                	"....0...\n" +
+                	"...BW0..\n" +
+                	"..0WB...\n" +
+                	"...0....\n" +
+                	"........\n" +
+                	"........\n" +
+                	"B" }
+           });
+    }
+    
+    
+    @Parameter 
+    public String boardInput;
 
-	@Test
-	public void legalMovesShouldAnswerStringWithLegalPositionsForBlack() {
-		String input = 	""+
-				"........\n" +
-				"........\n" +
-				"........\n" +
-				"...BW...\n" +
-				"...WB...\n" +
-				"........\n" +
-				"........\n" +
-				"........\n" +
-				"B";
-
-		String output =	""+
-				"........\n" +
-				"........\n" +
-				"....0...\n" +
-				"...BW0..\n" +
-				"..0WB...\n" +
-				"...0....\n" +
-				"........\n" +
-				"........\n" +
-				"B";
-		Reversi reversi = new Reversi(); 
-		assertEquals(output, reversi.printLegalMoves(input));
-	}
-	
-	@Test
-	public void legalMovesShouldAnswerStringWithLegalPositionsForWhite() {
-		String input = 	
-				"........\n" +
-				"........\n" +
-				"........\n" +
-				"...BW...\n" +
-				"...WB...\n" +
-				"........\n" +
-				"........\n" +
-				"........\n" +
-				"W";
-
-		String output =	
-				"........\n" +
-				"........\n" +
-				"...0....\n" +
-				"..0BW...\n" +
-				"...WB0..\n" +
-				"....0...\n" +
-				"........\n" +
-				"........\n" +
-				"W";
-		Reversi reversi = new Reversi(); 
-		assertEquals(output, reversi.printLegalMoves(input));
-	}
-
-
+    @Parameter (value=1)
+    public String boardOutputExpected;
+    
+    @Test
+    public void printLegalMovesShouldAnswerLegalMovesFromInput() {
+    	Reversi reversi = new Reversi(); 
+		assertEquals(this.boardOutputExpected, reversi.printLegalMoves(this.boardInput));
+    }
 }
