29ed580d17e1bb4605425f580da6acb162009227
Laurent Laffont <laurent.laffont@gmail.com>
2013-11-12 12:01:24 +0100

    Suppression premiers tests sur la grille Ajout tests grilles plus simple Premier cas de détection d'une position possible

--- src/Reversi.java
+++ src/Reversi.java
@@ -1,31 +1,42 @@
 
 public class Reversi {
-
 	public String printLegalMoves(String input) {
+		String[] inputRows = input.split("\n");
+		String outputRows = "";
+		String turn = "B";
 		if (this.isTurnForWhite(input))
-			return 
-					"........\n" +
-					"........\n" +
-					"...0....\n" +
-					"..0BW...\n" +
-					"...WB0..\n" +
-					"....0...\n" +
-					"........\n" +
-					"........\n" +
-					"W";
+			turn = "W";
 		
-		return 	"........\n" +
-				"........\n" +
-				"....0...\n" +
-				"...BW0..\n" +
-				"..0WB...\n" +
-				"...0....\n" +
-				"........\n" +
-				"........\n" +
-				"B";
+		for (int i = 0; i < inputRows.length - 1; i++) {
+			outputRows = outputRows + this.injectLegalMovesForRow(inputRows[i]) + "\n";
+		}
+		
+		return outputRows + turn;
 	}
 
 	
+	private String injectLegalMovesForRow(String row) {
+		Boolean shouldAddLegalMove = false;
+		String resultRow = "";
+		
+		for (int i = 0; i < row.length(); i++) {
+			Character cell = row.charAt(i);
+			Character resultCell = cell;
+			
+			if ('W' == cell)
+				shouldAddLegalMove = true;
+			
+			if (('.' == cell) && shouldAddLegalMove) {
+				shouldAddLegalMove = false;
+				resultCell = '0';
+			}
+			
+			resultRow = resultRow + resultCell.toString();
+		}
+		return resultRow;
+	}
+
+
 	private boolean isTurnForWhite(String input) {
 		return input.charAt(input.length() - 1) == 'W';
 	}
