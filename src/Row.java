4cf05a92f181e85332b35cfdda4eaa11388c0eda
Laurent Laffont <laurent.laffont@gmail.com>
2013-11-13 10:05:45 +0100

    Refacto nettoyage Row

--- src/Row.java
+++ src/Row.java
@@ -1,40 +1,20 @@
 
 public class Row {
-
 	private String inputRow;
-	private boolean shouldAddLegalMove;
 
 	public Row(String row) {
 		this.inputRow = row;
 	}
 
 	public void printLegalMovesOn(ReversiGUI gui) {
-		this.shouldAddLegalMove = false;
-
-		Cell cell = new Cell(this.inputRow.charAt(0));
-		cell.printLegalMoveOn(gui);
-	//	gui.appendCell(this.inputRow.charAt(0));
-		for (int i = 1; i < this.inputRow.length(); i++) {
+		Cell cell = null;
+		
+		for (int i = 0; i < this.inputRow.length(); i++) {
 			cell = new Cell(this.inputRow.charAt(i), cell);
 			cell.printLegalMoveOn(gui);
-		//	this.printCellOn(this.inputRow.charAt(i), gui);
 		}
 		
 		gui.endRow();
-	}
-
-	
-	private void printCellOn(Character cell, ReversiGUI gui) {
-		if ('W' == cell)
-			shouldAddLegalMove = true;
-		
-		if (('.' == cell) && shouldAddLegalMove) {
-			shouldAddLegalMove = false;
-			gui.appendCell('0');
-			return;
-		}
-		
-		gui.appendCell(cell);
 	}
 
 }
